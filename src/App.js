b95e0c8165ac988debf9d72319823c52e38f8a26
Iv-Alex <web@logycon.ru>
2021-07-08 18:09:01 +0300

    predeploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
+import React, { Component } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AddProject from "./components/add-project.component";
+import Project from "./components/project.component";
+import ProjectsList from "./components/projects-list.component";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-expand navbar-dark bg-dark">
+                    <a href="/projects" className="navbar-brand">
+                        <img src={logo} className="App-logo" alt="logo" />
+                    </a>
+                    <div className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <Link to={"/projects"} className="nav-link">
+                                Projects
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to={"/add"} className="nav-link">
+                                Add
+                            </Link>
+                        </li>
+                    </div>
+                </nav>
+
+                <div className="container mt-3">
+                    <Switch>
+                        <Route exact path={["/", "/projects"]} component={ProjectsList} />
+                        <Route exact path="/add" component={AddProject} />
+                        <Route path="/projects/:id" component={Project} />
+                    </Switch>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/services/project.service.js
@@ -0,0 +1,33 @@
+import http from "../http-common";
+
+class ProjectDataService {
+    getAll() {
+        return http.get("/projects");
+    }
+
+    get(id) {
+        return http.get(`/projects/${id}`);
+    }
+
+    create(data) {
+        return http.post("/projects", data);
+    }
+
+    update(id, data) {
+        return http.put(`/projects/${id}`, data);
+    }
+
+    delete(id) {
+        return http.delete(`/projects/${id}`);
+    }
+
+    deleteAll() {
+        return http.delete(`/projects`);
+    }
+
+    findByTitle(title) {
+        return http.get(`/projects?title=${title}`);
+    }
+}
+
+export default new ProjectDataService();
